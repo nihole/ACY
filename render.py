9e47b02840faa98e0066a2f8e2222e92187796b8
relokhin <relokhin@cisco.com>
2019-05-25 10:04:45 +0000

    compile.py -> render.py

--- render.py
+++ render.py
@@ -3,19 +3,21 @@ import sys
 import yaml
 import jinja2
 
-    ######### get file's names ####################
+    ######### get file's names from the command line ####################
 if (len(sys.argv)==3):
     j2_file = sys.argv[1]
     yaml_file = sys.argv[2]
 else:
     print ("   ######################################################\n")
-    print ("   python3 ../compile.pl template.j2 device_name.yml\n")
+    print ("   Syntax is:\n")
+    print ("   python3 render.py template_file.j2 configuration_yaml_file.yml\n")
     print ("   ######################################################\n")
     quit()
 
 loader = jinja2.FileSystemLoader('/tmp')
 env = jinja2.Environment(autoescape=True, loader=loader)
 
+   ######### take data from YAML file ####################
 my_config=''
 f = open( "./%s" % yaml_file )		
 data1 = f.read()
@@ -23,18 +25,17 @@ f.close()
 
 yaml_data = yaml.load( data1 )
 
+   ######### read templates from Jinja2 file ####################
 f = open( "./%s" % j2_file )
 data2 = f.read()
 f.close()
 
 template = env.from_string(data2)
 
+   ######### YAML data + Jinja data -> configuration ####################
 my_config = template.render(yaml_data);
-
-#my_config = "".join([s for s in my_config.splitlines(True) if s.strip("\r\n")])
 
 # Remove blank lines:
 my_config = "".join([s for s in my_config.splitlines(True) if (not re.search("^\s*$", s))])
 
 print(my_config)
-
