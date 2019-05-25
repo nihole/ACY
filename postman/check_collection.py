4e58ecbdfd8a73fa9011cce75802de3ae13b15bd
relokhin <relokhin@cisco.com>
2019-05-25 10:22:35 +0000

    Some comments added to python scripts

--- postman/check_collection.py
+++ postman/check_collection.py
@@ -7,15 +7,21 @@ if (len(sys.argv)==2):
     yaml_file = sys.argv[1]
 else:
     print ("   ######################################################\n")
-    print ("   python3 check_collection.py collection.yml\n")
+    print ("   Syntax is:\n")
+    print ("   python3 check_collection.py collection_file.yml\n")
     print ("   ######################################################\n")
     quit()
 
+
+   ######### take data from YAML file ####################
 
 my_config=''
 f = open( "./%s" % yaml_file )		
 data1 = f.read()
 f.close()
+
+   ######### Take the data from files being pointed in "path". Only if action == "yes" ######
+   ######### Print the name of API request and path to the file ######
 
 print ("\n> These POST API requests will be used:\n")
 print ("####################################################\n")
--- postman/create_collection.py
+++ postman/create_collection.py
@@ -9,18 +9,21 @@ def take_data(path):
     fl.close
     return data
 
-######### get file's names ####################
+######### get file's names from the command line ###################
 if (len(sys.argv)==3):
     j2_file = sys.argv[1]
     yaml_file = sys.argv[2]
 else:
     print ("   ######################################################\n")
-    print ("   python3 create_collection template.j2 collection.yml\n")
+    print ("   Syntax is:\n")
+    print ("   python3 create_collection.py template_file.j2 collection_file.yml\n")
     print ("   ######################################################\n")
     quit()
 
 loader = jinja2.FileSystemLoader('/tmp')
 env = jinja2.Environment(autoescape=False, loader=loader)
+
+   ######### take data from YAML file ####################
 
 my_config=''
 f = open( "./%s" % yaml_file )		
@@ -28,6 +31,9 @@ data1 = f.read()
 f.close()
 
 yaml_data = yaml.load( data1 )
+
+   ######### Take data from YAML. Only if action == "yes"  ####################
+
 for j in yaml_data["files"]:
     action = "no"
     if (not j["action"]):
@@ -41,7 +47,7 @@ for j in yaml_data["files"]:
         j_str = j_str.replace('\"', '\\"').replace('\t','\\t').replace('\n','\\n')
         j["path"] = str(j_str)
 
-
+   ######### read templates from Jinja2 file ####################
 
 f = open( "./%s" % j2_file )
 data2 = f.read()
@@ -49,16 +55,19 @@ f.close()
 
 template = env.from_string(data2)
 
+   ######### YAML data + Jinja data -> POSTMAN collection ####################
+
 my_config = template.render(yaml_data);
 
+   ######### Some additional editing ####################
 
 # Remove blank lines:
 my_comfig = [s for s in my_config.splitlines(True) if (not re.search("^\s*$", s))]
 
-# Remove comma 
+# We also have to remove one comma (third line from the end of file)
 my_comfig[-3] = my_comfig[-3].replace(",","")
 
-# Create str
+# Create string
 my_config_txt = "".join(my_comfig)
 
 print(my_config_txt)
